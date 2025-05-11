c7bce2a19faecae0500d21eba1cf16a3c751ec3d
suetaYT <minaumov02.17@gmail.com>
2025-05-11 15:45:19 +0300

    to server edit

--- get_ssl_cert.ps1
+++ get_ssl_cert.ps1
@@ -13,16 +13,19 @@ docker-compose up -d frontend
 
 # Даем nginx несколько секунд на запуск
 Write-Host "Waiting for nginx to start..."
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 10
 
 # Запросить сертификат
 Write-Host "Requesting SSL certificate..."
 docker-compose run --rm certbot certonly --webroot -w /var/www/certbot `
   --email minaumov02.17@yandex.ru -d шеш-беш.рф -d xn--e1afchk6c7d.xn--p1ai --agree-tos --no-eff-email
 
-# Перезапустить все сервисы
-Write-Host "Restarting all services..."
+# Перезапустить все сервисы для применения SSL
+Write-Host "Restarting all services to apply SSL configuration..."
 docker-compose down
 docker-compose up -d
 
-Write-Host "SSL certificate installation completed. Your site should now be available at https://шеш-беш.рф" 
+Write-Host "SSL certificate installation completed."
+Write-Host "Your site should now be available at:"
+Write-Host "http://шеш-беш.рф (will redirect to HTTPS if certificate was obtained)"
+Write-Host "https://шеш-беш.рф (if certificate was successfully obtained)" 
